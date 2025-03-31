b0569f83b2c2a6d05f06b3e7d848c460ed30fafc
mariejoycesantiago <marie.j.b.santiago@gmail.com>
2025-03-31 15:32:40 +0000

    Please work

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,6 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-import streamlit as st
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
@@ -34,11 +32,11 @@ if ingredients_list:
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'search_on'].iloc[0]
-        st.write ('The search value for ',fruit_choosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'SEARCH_ON'].iloc[0]
+        #st.write ('The search value for ',fruit_choosen,' is ', search_on, '.')
 
         st.subheader(fruit_choosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_list)
