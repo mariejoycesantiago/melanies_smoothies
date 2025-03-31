d8a141fde5dc757176a37a069b5b5399d1dc84d5
mariejoycesantiago <marie.j.b.santiago@gmail.com>
2025-03-31 15:24:24 +0000

    Last update panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,12 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'search_on'].iloc[0]
+        st.write ('The search value for ',fruit_choosen,' is ', search_on, '.')
+
         st.subheader(fruit_choosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_list)
